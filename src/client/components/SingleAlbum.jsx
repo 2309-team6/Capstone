a413411400c9d1e6317cb2a20c568e69a5370a18
HKGilchrist <hulseyckirk@gmail.com>
2023-11-29 21:24:09 -0700

    Fetch single album function

--- src/client/components/SingleAlbum.jsx
+++ src/client/components/SingleAlbum.jsx
@@ -1,13 +1,56 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { useParams } from "react-router-dom";
 
 let API = "http://localhost:3000/api/";
 
 function SingleAlbum() {
-  const [count, setCount] = useState(0);
+  const [album, setAlbum] = useState({});
+
+  const { id } = useParams(); 
+
+  useEffect(() => {
+    fetchSingleAlbum();
+  }, []);
+
+  async function fetchSingleAlbum() {
+
+    try{
+      const { data: json } = await axios.get(`${API}/albums/${id}`); 
+      
+      setAlbum(json.album); 
+    }
+    catch (err) {
+      console.error("Unable to find that album: ", err.message);
+    }
+  }
+
+  // TO DO: Add - Reviews & Comments
 
   return (
-    <h1>Single Album</h1>
+    <div className="single-album">
+          <h1>Single Album</h1>
+
+      {/* {album.title ? (
+          <>
+            <h2>{album.title}</h2>
+            <h3>By: {album.artist}</h3>
+            <h2>Genre: {album.genre}</h2>
+            <h2>Release Date: {album.releasedate}</h2>
+
+            <img src={album.imgurl} alt={album.title} />
+          </>
+        ) : (
+          <p>Loading...</p>
+        )} */}
+
+          {/* <h2>{album.title}</h2>
+          <h3>By: {album.artist}</h3>
+          <h2>Genre: {album.releasedate}</h2>
+          <h2>Release Date: {album.releasedate}</h2>
+          <img src={album.imgurl} /> */}
+         
+    </div>
   );
 }
 
