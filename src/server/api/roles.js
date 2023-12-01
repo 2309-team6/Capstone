e48d282dafefda31695b4cbe323c10fdd9b10431
HKGilchrist <hulseyckirk@gmail.com>
2023-11-30 17:12:45 -0700

    Fixing capitialization issues

--- src/server/api/roles.js
+++ src/server/api/roles.js
@@ -1,6 +1,6 @@
 const isLoggedIn = (...expectedRole) => {
   return (req, res, next) => {
-    if (expectedRole?.includes(req?.user?.role)) {
+    if (expectedRole?.includes(req?.user?.role.toLowerCase())) {
       next();
     } else {
       res.status(403).json({ message: "Unauthorized" });
