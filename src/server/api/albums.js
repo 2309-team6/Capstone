5b13b99145649b2698b00edcb50a9a26ec912127
Phillip Sim <phillipsim@phillips-air.mynetworksettings.com>
2023-12-01 14:39:54 -0500

    get album by id and get all comments

--- src/server/api/albums.js
+++ src/server/api/albums.js
@@ -1,4 +1,16 @@
 const express = require("express");
 const albumRouter = express.Router();
 
+const {getAlbum} = require('../db/albums');
+
+albumRouter.get('/:id', async (req, res, next) => {
+  try {
+    const album = await getAlbum(req.params.id);
+    res.send(album);
+  }catch (err) {
+    next(err);
+  }
+});
+
+
 module.exports = albumRouter;
