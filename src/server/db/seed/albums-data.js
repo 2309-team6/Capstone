b5c165ee07ba2ed8e8b6d6483e6b9dae0b502c64
HKGilchrist <140447822+HKGilchrist@users.noreply.github.com>
2023-12-01 08:41:24 -0700

    DB Set Up Changes (#3)
    * Added roles to users
    
    * Seeding DB with albums, album reviews.
    
    * Seeding User Comments table.
    
    * Removing favorites table.
    
    * Setting up API files.

---
+++ src/server/db/seed/albums-data.js
@@ -0,0 +1,11 @@
+const albums = [
+  {
+    title: "Rumors",
+    artist: "Fleetwood Mac",
+    genre: "rock",
+    releaseDate: 1977,
+    imgUrl: "https://upload.wikimedia.org/wikipedia/en/f/fb/FMacRumours.PNG",
+  },
+];
+
+module.exports = { albums };
