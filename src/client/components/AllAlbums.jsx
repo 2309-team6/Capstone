b0beb75eaef8a17b617544c2bca420dd3b5061d5
Steven <stevenwu4444@gmail.com>
2023-12-05 12:11:54 -0500

    updated AllAlbums.jsx for homepage

---
+++ src/client/components/AllAlbums.jsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Axios from 'axios';
+
+function AllAlbums() {
+  const [albums, setAlbums] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchAlbums();
+  }, []);
+
+  async function fetchAlbums() {
+    let API = 'http://localhost:3000/api/albums';
+
+    try {
+      const { data: response } = await Axios.get(`${API}`);
+      setAlbums(response); // Assuming the array is directly in the response
+    } catch (err) {
+      console.error(err.message);
+    }
+  }
+
+  return (
+    <ul className='albums-container'>
+      {albums.length ? (
+        albums.map(album => (
+          <li key={album.id}>
+            <h3>{album.title}</h3>
+            <h3>#{album.id}</h3>
+            <img src={album.imageUrl} />
+            <button onClick={() => navigate(`/details/${album.id}`)}>Show Details</button>
+          </li>
+        ))
+      ) : (
+        <h2>Loading ...</h2>
+      )}
+    </ul>
+  );
+}
+
+export default AllAlbums;
