cf320b6e011ff269b8f03b0232c9902dd31fa1fe
Steven <stevenwu4444@gmail.com>
2023-12-07 18:45:55 -0500

    fixed error

--- src/client/components/AllAlbums.jsx
+++ src/client/components/AllAlbums.jsx
@@ -1,11 +1,10 @@
-
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Axios from 'axios';
 import SearchBar from '../components/SearchBar';
 import FilteredAlbums from '../components/FilteredAlbums';
 
-function AllAlbums({ albums, setAlbums, filteredAlbums }) {
+function AllAlbums({ albums, setAlbums }) {
   const navigate = useNavigate();
   const [originalAlbums, setOriginalAlbums] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -15,17 +14,16 @@ function AllAlbums({ albums, setAlbums, filteredAlbums }) {
   }, []);
 
   async function fetchAlbums() {
-    let API = "http://localhost:3000/api/albums";
+    let API = 'http://localhost:3000/api/albums';
 
     try {
       const { data: response } = await Axios.get(`${API}`);
       setAlbums(response);
-      setOriginalAlbums(response); // Save the original Album list
+      setOriginalAlbums(response); 
     } catch (err) {
       console.error(err.message);
     }
   }
-
 
   const handleSearch = async (term) => {
     setSearchTerm(term);
@@ -38,7 +36,6 @@ function AllAlbums({ albums, setAlbums, filteredAlbums }) {
         console.error(err.message);
       }
     } else {
-      // If no search term, revert to the original list
       setAlbums(originalAlbums);
     }
   };
@@ -72,25 +69,6 @@ function AllAlbums({ albums, setAlbums, filteredAlbums }) {
         </ul>
       )}
     </div>
-
-  async function onNavigate(albumId) {
-    event.preventDefault();
-    navigate(`/albums/${albumId}`);
-  }
-
-  return (
-    <ul className="albums-container">
-      {(filteredAlbums.length ? filteredAlbums : albums).map((album) => (
-        <li key={album.id} className="all-albums-details">
-          <h2>{album.title}</h2>
-          {/* <h3>#{album.id}</h3> */}
-          <h3>{album.artist}</h3>
-          {/* <h3>{album.releasedate}</h3> */}
-          <img src={album.imgurl} alt={album.title} className="all-album-img" />
-          <button onClick={() => onNavigate(album.id)}>Show Details</button>
-        </li>
-      ))}
-    </ul>
   );
 }
 
