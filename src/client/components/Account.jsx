3c6a0f4a67c17365c520bd3a7c980aa343ab6b93
Phillip Sim <phillipsim@phillips-air.mynetworksettings.com>
2023-12-07 14:55:11 -0500

    fixed usestate issues on account component

--- src/client/components/Account.jsx
+++ src/client/components/Account.jsx
@@ -1,157 +1,146 @@
 import { useState, useEffect } from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 let API = "http://localhost:3000/api/";
 
 function Account(props) {
+  const [user, setUser] = useState([]);
+  const [myComments, setMyComments] = useState([]);
+  const [myReviews, setMyReviews] = useState([]);
+  const [error, setError] = useState(null);
 
-  const [user, setUser] = useState({})
-  const [myComments, setMyComments] = useState([])
-  const [myReviews, setMyReviews] = useState([])
-
-  const [error, setError] = useState(null)
-
-  useEffect (() => {
-      // tokenTest()
-      if (props) {
-        fetchMyAccount();
-        console.log("user.id: ", user.id)
-        fetchMyAccountDetails(user.id);
-      }
-    
-  }, [token])
-
-
-  async function tokenTest (){
-    if(props){
-      fetchMyAccount()
-      fetchMyAccountDetails()
+  useEffect(() => {
+    if (props.token) {
+      fetchMyAccount();
+      // console.log("user.id: ", user.id);
+      
     }
-  }
+  }, [props.token]);
+
+  // async function tokenTest() {
+  //   if (props) {
+  //     fetchMyAccount();
+  //     fetchMyAccountDetails();
+  //   }
+  // }
 
   // need this function to initally grab user info (user.id)
-  async function fetchMyAccount(){
+  async function fetchMyAccount() {
     try {
-
-      const response = await fetch (`${API}/users/info`, {
+      const response = await fetch(`${API}/users/info`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${props?.token}`,
         },
-      })
+      });
 
-      const json = await response.json()
+      const json = await response.json();
 
-      console.log("user: ", json)
-      setUser(json) //maybe return here?
+      console.log("user: ", json);
+      setUser(json); //maybe return here?
+      // console.log("usestate user: ", user)
+      // console.log("user.id: ", user.id)
+      if (json.id) {
+        fetchMyAccountDetails(json.id);
+      }
       
-    } catch(error) {
-      setError(error.message)
+    } catch (error) {
+      setError(error.message);
     }
   }
 
   // use this function with the user.id from other function to grab comment and review data
-  async function fetchMyAccountDetails(id){
+  async function fetchMyAccountDetails(id) {
     try {
-
-      const response = await fetch (`${API}/users/details/${id}`, {
+      const response = await fetch(`${API}/users/details/${id}`, {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${props?.token}`,
         },
-      })
+      });
 
-      const json = await response.json()
-      
-      setMyComments(json.comments)
-      setMyReviews(json.reviews)
+      const json = await response.json();
 
-      console.log("json: ", json)
-      console.log("json.comments: ", myComments)
-      console.log("json.reviews: ", myReviews)
-      
-    } catch(error) {
-      setError(error.message)
+      setMyComments(json.comments);
+      setMyReviews(json.reviews);
+      // json is an object
+      console.log("json: ", json);
+
+
+    } catch (error) {
+      setError(error.message);
     }
   }
 
   async function deleteComment(id) {
-    try{
-
-      const response = await fetch(`${API}/comments/${id}`,
-      {
+    try {
+      const response = await fetch(`${API}/comments/${id}`, {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${props?.token}`,
         },
-      })
-      const json = await response.json()
-      
-      fetchMyAccountDetails()
+      });
+      const json = await response.json();
 
-    } catch(error) {
-      setError(error.message)
+      fetchMyAccountDetails(user.id);
+    } catch (error) {
+      setError(error.message);
     }
   }
 
-  async function editComment(id){
-    try{
-      const response = await fetch (`${API}/comments/${id}`, {
+  async function editComment(id, updatedComment) {
+    try {
+      const response = await fetch(`${API}/comments/${id}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${props?.token}`,
         },
         body: JSON.stringify({
           updatedComment,
-        })
-      })
+        }),
+      });
 
-      const json = await response.json()
-      console.log("patch request response: ", json)
-   
-      fetchMyAccountDetails()
+      const json = await response.json();
+      console.log("patch request response: ", json);
 
-    }catch(error){
-      console.error(error.message)
+      fetchMyAccountDetails(user.id);
+    } catch (error) {
+      console.error(error.message);
     }
-
   }
 
-
   return (
-
-    <div className='account-page-container'>
-
+    <div className="account-page-container">
       {error && <p>{error}</p>}
-       
-          <h1>Welcome, {user.name}</h1>
-          
-          {token ? 
-            <div>
-              <h2>My Comments</h2>
-            
-              <ul>
-                {myComments.map(comment=>(
-                  <li key = {comment?.id}>
-                    <h3>{comment}</h3>
-                    <button onClick={() => deleteComment(comment?.id)}>Delete</button>
-                    <button onClick={() => editComment(comment?.id)}>Edit</button>
-                  </li>
-               ))}
-              </ul>
 
-            </div>
-            :
-            <p>It seems you are not logged in, click here: <Link to='/login'>Login</Link></p>
-          }
+      <h1>Welcome, {user.name}</h1>
 
+      {props.token ? (
+        <div>
+          <h2>My Comments</h2>
+          <ul>
+            {myComments.map((comment) => (
+              <li key={comment?.id}>
+                <h3>{comment.content}</h3>
+                <button onClick={() => deleteComment(comment?.id)}>
+                  Delete
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>
+          It seems you are not logged in, click here:{" "}
+          <Link to="/login">Login</Link>
+        </p>
+      )}
     </div>
-
   );
 }
 
